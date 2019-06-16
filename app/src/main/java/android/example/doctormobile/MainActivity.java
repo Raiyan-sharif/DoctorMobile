16eb3de3835ce78efb9b357c0a319b4c780e32ee
Raiyan <raiyan.sharif.1234@gmail.com>
2019-06-16 09:54:50 +0600

    Login page

--- app/src/main/java/android/example/doctormobile/MainActivity.java
+++ app/src/main/java/android/example/doctormobile/MainActivity.java
@@ -2,12 +2,28 @@ package android.example.doctormobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText email;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signInBtn = (Button) findViewById(R.id.sign_in_btn);
+        email = (EditText) findViewById(R.id.email_etv);
+        password = (EditText) findViewById(R.id.password_etv);
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
