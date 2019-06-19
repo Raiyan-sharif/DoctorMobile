142912b492659b48b7e408c5b460e9c0c1f209fd
Raiyan <raiyan.sharif.1234@gmail.com>
2019-06-19 13:11:48 +0600

    Basic improvement

--- app/src/main/java/android/example/doctormobile/FeverCause.java
+++ app/src/main/java/android/example/doctormobile/FeverCause.java
@@ -2,6 +2,10 @@ package android.example.doctormobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class FeverCause extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class FeverCause extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fever_cause);
+        final CheckBox c1 = (CheckBox)findViewById(R.id.checkBox1);
+        Button checkResultBtn = (Button)findViewById(R.id.check_result_btn);
+
+        checkResultBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                String a = c1.getText().toString();
+//                Toast.makeText(getApplicationContext(),"Em"+c1.isChecked(),Toast.LENGTH_LONG).show();
+
+                if(c1.isChecked()){
+                    Toast.makeText(getApplicationContext(),"ok"+c1.isChecked(),Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
     }
 }
