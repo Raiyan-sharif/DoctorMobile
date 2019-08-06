38b0782caa2e0bcb17a659e23096dcaff6b28681
Raiyan <raiyan.sharif.1234@gmail.com>
2019-08-06 07:07:28 +0600

    BMI works

--- app/src/main/java/android/example/doctormobile/BMI.java
+++ app/src/main/java/android/example/doctormobile/BMI.java
@@ -24,265 +24,9 @@ public class BMI extends AppCompatActivity {
         String[] heightAttr={"Meter","Inc"};
         String[] weightAttr={"Kg","lb"};
         bmiResult = (TextView)findViewById(R.id.cal_BMI_tv);
-//        final Spinner spinHeight = findViewById(R.id.Height_spinner);
-//        final Spinner spinWeight = findViewById(R.id.weight_spinner);
-//        spinHeight.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
-//        spinWeight.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
-
-//Creating the ArrayAdapter instance having the bank name list
-
-        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,heightAttr);
-        ArrayAdapter bb = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,weightAttr);
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        bb.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//Setting the ArrayAdapter data on the Spinner
-//        spinHeight.setAdapter(aa);
-//        spinWeight.setAdapter(bb);
         edtxtHeight = findViewById(R.id.edt_height);
         edtxtWeight = findViewById(R.id.edt_weight);
 
-//        spinWeight.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                try {
-//
-//
-//                    edtxtHeight = (EditText) findViewById(R.id.edt_height);
-//                    height = Double.parseDouble(edtxtHeight.getText().toString());
-//                    edtxtWeight = (EditText) findViewById(R.id.edt_weight);
-//                    weight = Double.parseDouble(edtxtWeight.getText().toString());
-//                    if(height == 0.0){
-//
-//                        bmiResult.setText("BMI: 0.0");
-//                        bmiResult.setTextColor(getResources().getColor(R.color.UnderWeight));
-//                        Log.e("Error", "if: "+result);
-//                    }
-//                    else if(spinHeight.getSelectedItem().equals("Meter") && spinWeight.getSelectedItem().equals("Kg")){
-//
-//                        result = Math.round(weight / (height*height)*100.0)/100.0;
-//                        String resultStr = Double.toString(result);
-//                        if(result<=18.5){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.NormalBMI));
-//                            Log.e("Error", "if Under: "+resultStr);
-//                        }
-//                        else if(result <= 24.9 && result>18.5){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.UnderWeight));
-//                            Log.e("Error", "else if Normal: "+result);
-//
-//                        }
-//                        else if(result>=25.0){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.OverWeight));
-//                            Log.e("Error", "else if over: "+result);
-//
-//                        }
-//                        else{
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
-//                            Log.e("Error", "else: "+result);
-//
-//                        }
-//                    }
-//                    else if(spinHeight.getSelectedItem().equals("Inc") && spinWeight.getSelectedItem().equals("lb")){
-//
-//                        result = Math.round(weight / (height*height*703)*100.0)/100.0;
-//                        String resultStr = Double.toString(result);
-//                        if(result<=18.5){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.NormalBMI));
-//                            Log.e("Error", "if Under: "+resultStr);
-//                        }
-//                        else if(result <= 24.9 && result>18.5){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.UnderWeight));
-//                            Log.e("Error", "else if Normal: "+result);
-//
-//                        }
-//                        else if(result>=25.0){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.OverWeight));
-//                            Log.e("Error", "else if over: "+result);
-//
-//                        }
-//                        else{
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
-//                            Log.e("Error", "else: "+result);
-//
-//                        }
-//                    }
-//                    else if(spinHeight.getSelectedItem().equals("Inc") && spinWeight.getSelectedItem().equals("Kg")){
-//
-//                        result = Math.round((weight/2.2) / (height*height*703.0)*100.0)/100.0;
-//                        String resultStr = Double.toString(result);
-//                        if(result<=18.5){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.NormalBMI));
-//                            Log.e("Error", "if Under: "+resultStr);
-//                        }
-//                        else if(result <= 24.9 && result>18.5){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.UnderWeight));
-//                            Log.e("Error", "else if Normal: "+result);
-//
-//                        }
-//                        else if(result>=25.0){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.OverWeight));
-//                            Log.e("Error", "else if over: "+result);
-//
-//                        }
-//                        else{
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
-//                            Log.e("Error", "else: "+result);
-//
-//                        }
-//                    }
-//                    else{
-//                        result = Math.round(weight / (height*height)*100.0)/100.0;
-//                        String resultStr = Double.toString(result);
-//                        Log.e("Error", "else else: "+result);
-//                        bmiResult.setText("BMI: "+resultStr);
-//                        bmiResult.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
-//                    }
-//
-//                }catch (Exception ex){
-//                    Log.e("Error", "Report: "+ex.getMessage());
-//
-//                }
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) {
-//
-//            }
-//        });
-
-//        spinHeight.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                try {
-//
-//
-//                    edtxtHeight = (EditText) findViewById(R.id.edt_height);
-//                    height = Double.parseDouble(edtxtHeight.getText().toString());
-//                    edtxtWeight = (EditText) findViewById(R.id.edt_weight);
-//                    weight = Double.parseDouble(edtxtWeight.getText().toString());
-//                    if(height == 0.0){
-//
-//                        bmiResult.setText("BMI: 0.0");
-//                        bmiResult.setTextColor(getResources().getColor(R.color.UnderWeight));
-//                        Log.e("Error", "if: "+result);
-//                    }
-//                    else if(spinHeight.getSelectedItem().equals("Meter") && spinWeight.getSelectedItem().equals("Kg")){
-//
-//                        result = Math.round(weight / (height*height)*100.0)/100.0;
-//                        String resultStr = Double.toString(result);
-//                        if(result<=18.5){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.NormalBMI));
-//                            Log.e("Error", "if Under: "+resultStr);
-//                        }
-//                        else if(result <= 24.9 && result>18.5){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.UnderWeight));
-//                            Log.e("Error", "else if Normal: "+result);
-//
-//                        }
-//                        else if(result>=25.0){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.OverWeight));
-//                            Log.e("Error", "else if over: "+result);
-//
-//                        }
-//                        else{
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
-//                            Log.e("Error", "else: "+result);
-//
-//                        }
-//                    }
-//                    else if(spinHeight.getSelectedItem().equals("Inc") && spinWeight.getSelectedItem().equals("lb")){
-//
-//                        result = Math.round(weight / (height*height*703)*100.0)/100.0;
-//                        String resultStr = Double.toString(result);
-//                        if(result<=18.5){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.NormalBMI));
-//                            Log.e("Error", "if Under: "+resultStr);
-//                        }
-//                        else if(result <= 24.9 && result>18.5){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.UnderWeight));
-//                            Log.e("Error", "else if Normal: "+result);
-//
-//                        }
-//                        else if(result>=25.0){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.OverWeight));
-//                            Log.e("Error", "else if over: "+result);
-//
-//                        }
-//                        else{
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
-//                            Log.e("Error", "else: "+result);
-//
-//                        }
-//                    }
-//                    else if(spinHeight.getSelectedItem().equals("Inc") && spinWeight.getSelectedItem().equals("Kg")){
-//
-//                        result = Math.round((weight/2.2) / (height*height*703)*100.0)/100.0;
-//                        String resultStr = Double.toString(result);
-//                        if(result<=18.5){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.NormalBMI));
-//                            Log.e("Error", "if Under: "+resultStr);
-//                        }
-//                        else if(result <= 24.9 && result>18.5){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.UnderWeight));
-//                            Log.e("Error", "else if Normal: "+result);
-//
-//                        }
-//                        else if(result>=25.0){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.OverWeight));
-//                            Log.e("Error", "else if over: "+result);
-//
-//                        }
-//                        else{
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
-//                            Log.e("Error", "else: "+result);
-//
-//                        }
-//                    }
-//                    else{
-//                        result = Math.round(weight / (height*height)*100.0)/100.0;
-//                        String resultStr = Double.toString(result);
-//                        Log.e("Error", "else else: "+result);
-//                        bmiResult.setText("BMI: "+resultStr);
-//                        bmiResult.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
-//                    }
-//
-//                }catch (Exception ex){
-//                    Log.e("Error", "Report: "+ex.getMessage());
-//
-//                }
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) {
-//
-//            }
-//        });
-
-        final String w ="";
-        final String h = "Inc";
 
         edtxtHeight.addTextChangedListener(new TextWatcher() {
             @Override
@@ -305,38 +49,10 @@ public class BMI extends AppCompatActivity {
                         bmiResult.setTextColor(getResources().getColor(R.color.UnderWeight));
                         Log.e("Error", "if: "+result);
                     }
-//                    else if(spinHeight.getSelectedItem().equals("Meter") && spinWeight.getSelectedItem().equals("Kg")){
-//
-//                        result = Math.round(weight / (height*height)*100.0)/100.0;
-//                        String resultStr = Double.toString(result);
-//                        if(result<=18.5){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.NormalBMI));
-//                            Log.e("Error", "if Under: "+resultStr);
-//                        }
-//                        else if(result <= 24.9 && result>18.5){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.UnderWeight));
-//                            Log.e("Error", "else if Normal: "+result);
-//
-//                        }
-//                        else if(result>=25.0){
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.OverWeight));
-//                            Log.e("Error", "else if over: "+result);
-//
-//                        }
-//                        else{
-//                            bmiResult.setText("BMI: "+resultStr);
-//                            bmiResult.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
-//                            Log.e("Error", "else: "+result);
-//
-//                        }
-//                    }
 
                     else if(height>0.0){
-
-                        result = Math.round((weight/2.2) / (height*height*703)*100.0)/100.0;
+                        double h2 = height * 0.0254;
+                        result = weight / (h2*h2);
                         String resultStr = Double.toString(result);
                         if(result<=18.5){
                             bmiResult.setText("BMI: "+resultStr);
@@ -363,7 +79,8 @@ public class BMI extends AppCompatActivity {
                         }
                     }
                     else{
-                        result = Math.round(weight / (height*height)*100.0)/100.0;
+                        double h2 = height * 0.0254;
+                        result = weight / (h2*h2);
                         String resultStr = Double.toString(result);
                         Log.e("Error", "else else: "+result);
                         bmiResult.setText("BMI: "+resultStr);
@@ -406,9 +123,10 @@ public class BMI extends AppCompatActivity {
                         Log.e("Error", "if: "+result);
                     }
 
-                    else if(height>0.0){
+                    else if(weight>0.0){
 
-                        result = Math.round((weight/2.2) / (height*height*703)*100.0)/100.0;
+                        double h2 = height * 0.0254;
+                        result = weight / (h2*h2);
                         String resultStr = Double.toString(result);
                         if(result<=18.5){
                             bmiResult.setText("BMI: "+resultStr);
@@ -435,7 +153,8 @@ public class BMI extends AppCompatActivity {
                         }
                     }
                     else{
-                        result = Math.round(weight / (height*height)*100.0)/100.0;
+                        double h2 = height * 0.0254;
+                        result = weight / (h2*h2);
                         String resultStr = Double.toString(result);
                         Log.e("Error", "else else: "+result);
                         bmiResult.setText("BMI: "+resultStr);
